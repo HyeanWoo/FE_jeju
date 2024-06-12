249845bce7abaa00351123103b3ec3ed39290436
HyeanWoo <wkdus7h@naver.com>
2024-06-12 13:51:01 +0000

    chore: add gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Call me By11{" "}
+            Call me By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
