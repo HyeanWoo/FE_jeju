30e7840a8e42814cef805f59c07118fb7fe77f32
HyeanWoo <wkdus7h@naver.com>
2024-06-12 13:41:57 +0000

    Create main.yml

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "바로제주 - 제주도 여행 정보",
+  description: "OTT에 나온 제주도의 아름다운 여행지를 알려드릴께요!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./FE_jeju/* ./output
+cp -R ./output ./FE_jeju/
