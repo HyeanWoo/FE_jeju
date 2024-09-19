de91779133a57f7298dc43f9dd669e920fd1bfb7
HyeanWoo <wkdus7h@naver.com>
2024-09-19 12:34:10 +0000

    fix: roll back

--- components/shared/KaKaoMap/KaKaoMap.tsx
+++ components/shared/KaKaoMap/KaKaoMap.tsx
@@ -1,26 +1,33 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+
+import React, { useEffect } from "react";
 
 const KakaoMap = () => {
-  const mapContainer = useRef(null);
-
   useEffect(() => {
-    if (!window?.kakao) {
-      return;
-    }
+    const kakaoMapScript = document.createElement("script");
+    kakaoMapScript.async = false;
+    kakaoMapScript.type = "text/javascript";
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=d189c92b8d450da84be516fa5364123b`;
+    document.head.appendChild(kakaoMapScript);
 
-    if (mapContainer.current) {
-      const options = {
-        // @ts-ignore
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
-      };
+    const onLoadKakaoAPI = () => {
       // @ts-ignore
-      new kakao.maps.Map(mapContainer.current, options);
-    }
+      window.kakao.maps.load(() => {
+        var container = document.getElementById("map");
+        var options = {
+          // @ts-ignore
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+        // @ts-ignore
+        var map = new window.kakao.maps.Map(container, options);
+      });
+    };
+
+    kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
   }, []);
 
-  return <div ref={mapContainer} style={{ width: 390, height: 195 }}></div>;
+  return <div id="map" style={{ width: 390, height: 195 }}></div>;
 };
 
 export default KakaoMap;
